7f7370db5907e5c08b32c5a39d50f8517f0c690a
OMAR K <mirxaumar1212@gmail.com>
2024-01-24 21:46:37 +0500

    7th commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import countries from './countries';
 import ProgressBar from './ProgressBar.jsx';
 import { saveAs } from 'file-saver';
 
+const shuffledCountries = [...countries].sort(() => Math.random() - 0.5);
+
 function App() {
   const [screen, setScreen] = useState('start');
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [showDetails, setShowDetails] = useState(false);
   const totalQuestions = 5;
   const [questionDetails, setQuestionDetails] = useState([]);
 
@@ -17,15 +18,14 @@ function App() {
   };
 
   const handleAnswerClick = (isCorrect, selectedOption) => {
+    const currentCountry = shuffledCountries[currentQuestionIndex];
     const details = {
       question: currentCountry,
       correctAnswer: currentCountry.name,
       userAnswer: isCorrect ? 'Correct!' : 'Incorrect!',
       selectedAnswer: selectedOption,
-
     };
     setQuestionDetails((prevDetails) => [...prevDetails, details]);
-
     setSelectedAnswer(isCorrect);
     setScore((prevScore) => (isCorrect ? prevScore + 1 : prevScore));
     setScreen('result');
@@ -46,25 +46,20 @@ function App() {
   const restartGame = () => {
     setCurrentQuestionIndex(0);
     setScore(0);
-    setQuestionDetails([]); 
+    setQuestionDetails([]);
     setScreen('question');
   };
 
   const showQuestionDetails = () => {
-    setShowDetails(true);
     setScreen('detail');
   };
 
   const hideQuestionDetails = () => {
-    setShowDetails(false);
     setScreen('end');
   };
 
-  const currentCountry = countries[currentQuestionIndex];
-  const answerOptions = getAnswerOptions();
-
-  function getAnswerOptions() {
-    const shuffledCountries = [...countries].sort(() => Math.random() - 0.5);
+  const getAnswerOptions = () => {
+    const currentCountry = shuffledCountries[currentQuestionIndex];
     const correctCountryIndex = shuffledCountries.findIndex(
       (country) => country.ISOCode === currentCountry.ISOCode
     );
@@ -73,176 +68,181 @@ function App() {
       .slice(0, 3);
     const randomIndex = Math.floor(Math.random() * 4);
     answerOptions.splice(randomIndex, 0, shuffledCountries[correctCountryIndex]);
-    console.log(answerOptions);
     return answerOptions;
+  };
 
-  }
+  const currentCountry = shuffledCountries[currentQuestionIndex];
+  const answerOptions = getAnswerOptions();
 
   const downloadDetailsAsCSV = () => {
     try {
-      const csvContent = "data:text/csv;charset=utf-8," +
+      const csvContent =
         "Quiz Question Details\n" +
         "Question,Correct Answer,Your Answer\n" +
-        questionDetails.map(detail =>
-          `${detail.question},${detail.correctAnswer},${detail.userAnswer}\n`
-        ).join('');
-
-      const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8' });
+        questionDetails
+          .map(
+            (detail) =>
+              `${detail.question.name},${detail.correctAnswer},${detail.userAnswer}\n`
+          )
+          .join('');
+      const blob = new Blob([csvContent], {
+        type: 'text/csv;charset=utf-8',
+      });
       saveAs(blob, 'question_details.csv');
     } catch (error) {
       console.error('Error generating or downloading CSV:', error);
-
     }
   };
 
+  const renderStartScreen = () => (
+    <div className="screen">
+      <div className="game-container">
+        <div className="header">
+          <h1>Flag Quiz</h1>
+          <button className="btn start" onClick={startGame}>
+            START GAME
+          </button>
+        </div>
+        <p>Test your knowledge of world flags!</p>
+      </div>
+    </div>
+  );
+
+  const renderQuestionScreen = () => (
+    <div className="game-container">
+      <div className="header">
+        <h1>Flag Quiz</h1>
+        <div className="scoreboard" style={{ marginLeft: "80px" }}>
+          <p className="score-title">Score</p>
+          <p className="score-num">{score}</p>
+        </div>
+        <div className="scoreboard">
+          <p className="score-title">Question</p>
+          <p className="score-num">{currentQuestionIndex + 1}</p>
+        </div>
+      </div>
+      <p className="game-status-display">Which country is represented by this flag?</p>
+      <ProgressBar round={currentQuestionIndex + 1} roundsTotal={totalQuestions} />
+      <div className="question-container">
+        <div className="flag">
+          <img
+            src={`https://flagcdn.com/80x60/${currentCountry.ISOCode}.png`}
+            alt={currentCountry.name}
+          />
+        </div>
+        <div className="answers-container">
+          {answerOptions.map((country, index) => (
+            <button
+              key={index}
+              className={`answer-button ${selectedAnswer !== null ? (country === currentCountry ? 'correct' : 'incorrect') : ''
+                }`}
+              onClick={() => handleAnswerClick(country === currentCountry, country.name)}
+            >
+              {country.name}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+
+  const renderResultScreen = () => (
+    <div className="game-container">
+      <div>
+        <p className="result-message">{selectedAnswer ? 'Correct!' : 'Incorrect!'}</p>
+        <p className="score">Score: {score}</p>
+        <button className="btn" onClick={nextQuestion}>
+          Next Question
+        </button>
+      </div>
+    </div>
+  );
+
+  const renderEndScreen = () => (
+    <div className="game-container">
+      <div className="header">
+        <h1>FLAGS QUIZ</h1>
+        <button className="btn start" onClick={restartGame}>
+          RESTART GAME
+        </button>
+      </div>
+      <p>Game Over</p>
+      <div className="game-status-display">
+        <div className="scoreboard">
+          <p className="score-title">Score</p>
+          <p className="score-num">{score}</p>
+        </div>
+        <div className="scoreboard">
+          <p className="score-title">Question</p>
+          <p className="score-num">{totalQuestions}</p>
+        </div>
+      </div>
+      <p className="final-score">Your Final Score: {Math.round((score / totalQuestions) * 100)}%</p>
+      {score === totalQuestions && (
+        <p className="congratulations">Perfect Score! Well Done!</p>
+      )}
+      {score < totalQuestions && (
+        <p className="encouragement">Keep Going! You can do better next time.</p>
+      )}
+      <div className="button-container">
+        <button className="btn start" onClick={restartGame}>
+          Try Again
+        </button>
+        <button className="btn start" onClick={showQuestionDetails}>
+          Details
+        </button>
+      </div>
+    </div>
+  );
+
+  const renderDetailScreen = () => (
+    <div className="details-screen">
+      <h2>Details</h2>
+      <div className="details-table-container">
+        <table className="details-table">
+          <thead>
+            <tr>
+              <th>Question</th>
+              <th>Selected answer</th>
+              <th>Your Answer</th>
+              <th>Correct Answer</th>
+            </tr>
+          </thead>
+          <tbody>
+            {questionDetails.map((detail, index) => (
+              <tr key={index}>
+                <td><img
+                  src={`https://flagcdn.com/40x30/${detail.question.ISOCode}.png`}
+                  alt={detail.question.name}
+                /></td>
+                <td>{detail.selectedAnswer}</td>
+                <td className={detail.userAnswer === 'Correct!' ? 'correct-answer' : 'incorrect-answer'}>
+                  {detail.userAnswer}
+                </td>
+                <td>{detail.correctAnswer}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="button-container">
+        <button className="btn" onClick={downloadDetailsAsCSV}>
+          Download
+        </button>
+        <button className="btn" onClick={hideQuestionDetails}>
+          Close Details
+        </button>
+      </div>
+    </div>
+  );
 
   return (
     <div className="screen">
-      {screen === 'start' && (
-        <div className="screen">
-          <div className="game-container">
-            <div className="header">
-              <h1>Flag Quiz</h1>
-              <button className="btn start" onClick={startGame}>
-                START GAME
-              </button>
-            </div>
-            <p>Test your knowledge of world flags!</p>
-          </div>
-        </div>
-      )}
-
-      {screen === 'question' && (
-        <div className="game-container">
-          <div className="header">
-            <h1>Flag Quiz</h1>
-            <div className="scoreboard" style={{ marginLeft: "80px" }}>
-              <p className="score-title">
-                Score
-              </p>
-              <p className="score-num">{score}</p>
-            </div>
-            <div className="scoreboard">
-              <p className="score-title">Question</p>
-              <p className="score-num">{currentQuestionIndex + 1}</p>
-            </div>
-          </div>
-          <p className="game-status-display">Which country is represented by this flag?</p>
-          <ProgressBar round={currentQuestionIndex + 1} roundsTotal={totalQuestions} />
-          <div className="question-container">
-            <div className="flag">
-              <img
-                src={`https://flagcdn.com/80x60/${currentCountry.ISOCode}.png`}
-                alt={currentCountry.name}
-              />
-            </div>
-            <div className="answers-container">
-              {answerOptions.map((country, index) => (
-                <button
-                  key={index}
-                  className={`answer-button ${selectedAnswer !== null ? (country === currentCountry ? 'correct' : 'incorrect') : ''
-                    }`}
-                  onClick={() => handleAnswerClick(country === currentCountry, country.name)}
-                >
-                  {country.name}
-                </button>
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
-
-      {screen === 'result' && (
-        <div className="game-container">
-          <div>
-            <p className="result-message">{selectedAnswer ? 'Correct!' : 'Incorrect!'}</p>
-            <p className="score">Score: {score}</p>
-            <button className="btn" onClick={nextQuestion}>
-              Next Question
-            </button>
-          </div>
-        </div>
-      )}
-
-      {screen === 'end' && (
-        <div className="game-container">
-          <div className="header">
-            <h1>FLAGS QUIZ</h1>
-            <button className="btn start" onClick={restartGame}>
-              RESTART
-               GAME
-            </button>
-          </div>
-          <p>Game Over</p>
-          <div className="game-status-display">
-            <div className="scoreboard">
-              <p className="score-title">Score</p>
-              <p className="score-num">{score}</p>
-            </div>
-            <div className="scoreboard">
-              <p className="score-title">Question</p>
-              <p className="score-num">{totalQuestions}</p>
-            </div>
-          </div>
-          <p className="final-score">Your Final Score: {Math.round((score / totalQuestions) * 100)}%</p>
-          {score === totalQuestions && (
-            <p className="congratulations">Perfect Score! Well Done!</p>
-          )}
-          {score < totalQuestions && (
-            <p className="encouragement">Keep Going! You can do better next time.</p>
-          )}
-          <div className="button-container">
-            <button className="btn start" onClick={restartGame}>
-              Try Again
-            </button>
-            <button className="btn start" onClick={showQuestionDetails}>
-              Details
-            </button>
-          </div>
-        </div>
-      )}
-
-      {screen === 'detail' && (
-        <div className="details-screen">
-          <h2>Details</h2>
-          <div className="details-table-container">
-            <table className="details-table">
-              <thead>
-                <tr>
-                  <th>Question</th>
-                  <th>Selected answer</th>
-                  <th>Your Answer</th>
-                  <th>Correct Answer</th>
-
-                </tr>
-              </thead>
-              <tbody>
-                {questionDetails.map((detail, index) => (
-                  <tr key={index}>
-                    <td><img
-                      src={`https://flagcdn.com/40x30/${detail.question.ISOCode}.png`}
-                      alt={detail.question.name}
-                    /></td>
-                    <td>{detail.selectedAnswer}</td>
-                    <td className={detail.userAnswer === 'Correct!' ? 'correct-answer' : 'incorrect-answer'}>
-                      {detail.userAnswer}
-                    </td>
-                    <td>{detail.correctAnswer}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="button-container">
-            <button className="btn" onClick={downloadDetailsAsCSV}>
-              Download CSV
-            </button>
-            <button className="btn" onClick={hideQuestionDetails}>
-              Close Details
-            </button>
-          </div>
-        </div>
-      )}
+      {screen === 'start' && renderStartScreen()}
+      {screen === 'question' && renderQuestionScreen()}
+      {screen === 'result' && renderResultScreen()}
+      {screen === 'end' && renderEndScreen()}
+      {screen === 'detail' && renderDetailScreen()}
     </div>
   );
 }

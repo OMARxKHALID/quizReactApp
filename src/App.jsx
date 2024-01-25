b28ef1b4777149f17fa77a100e651786e010da70
OMAR K <mirxaumar1212@gmail.com>
2024-01-26 02:56:18 +0500

    11th commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import countries from './countries';
 import ProgressBar from './ProgressBar.jsx';
 import { saveAs } from 'file-saver';
 
-function App() {
+const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+
+const App = () => {
   const [screen, setScreen] = useState('start');
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const totalQuestions = 5;
   const [questionDetails, setQuestionDetails] = useState([]);
   const [shuffledCountries, setShuffledCountries] = useState([]);
 
+  const totalQuestions = 5;
+
   const startGame = () => {
-    setShuffledCountries([...countries].sort(() => Math.random() - 0.5));
+    setShuffledCountries(shuffleArray([...countries]));
     setScreen('question');
   };
 
@@ -44,11 +47,12 @@ function App() {
     setCurrentQuestionIndex((prevIndex) =>
       prevIndex < totalQuestions - 1 ? prevIndex + 1 : 0
     );
+
     if (currentQuestionIndex === totalQuestions - 1) {
       setScreen('end');
     } else {
-      setShuffledCountries([...countries].sort(() => Math.random() - 0.5));
       setScreen('question');
+      setShuffledCountries(shuffleArray([...countries]));
     }
   };
 
@@ -65,23 +69,12 @@ function App() {
     const correctCountryIndex = shuffledCountries.findIndex(
       (country) => country.ISOCode === currentCountry.ISOCode
     );
-    const answerOptions = shuffledCountries
+    let answerOptions = shuffledCountries
       .filter((_, index) => index !== correctCountryIndex)
       .slice(0, 3);
     answerOptions.push(currentCountry);
-  
-    // Fisher-Yates (Knuth) Shuffle
-    for (let i = answerOptions.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [answerOptions[i], answerOptions[j]] = [answerOptions[j], answerOptions[i]];
-    }
-  
-    return answerOptions;
+    return shuffleArray(answerOptions);
   };
-  
-
-  const currentCountry = shuffledCountries[currentQuestionIndex];
-  const answerOptions = getAnswerOptions();
 
   const downloadDetailsAsCSV = () => {
     try {
@@ -117,51 +110,67 @@ function App() {
     </div>
   );
 
-  const renderQuestionScreen = () => (
-    <div className="game-container">
-      <div className="header">
-        <h1>Flag Quiz</h1>
-        <div className="scoreboard" style={{ marginLeft: "80px" }}>
-          <p className="score-title">Score</p>
-          <p className="score-num">{score}</p>
+  const renderQuestionScreen = () => {
+    const currentCountry = shuffledCountries[currentQuestionIndex];
+    const answerOptions = getAnswerOptions();
+
+    return (
+      <div className="game-container">
+        <div className="header">
+          <h1>Flag Quiz</h1>
+          <div className="scoreboard" style={{ marginLeft: "80px" }}>
+            <p className="score-title">Score</p>
+            <p className="score-num">{score}</p>
+          </div>
+          <div className="scoreboard">
+            <p className="score-title">Question</p>
+            <p className="score-num">{currentQuestionIndex + 1}</p>
+          </div>
         </div>
-        <div className="scoreboard">
-          <p className="score-title">Question</p>
-          <p className="score-num">{currentQuestionIndex + 1}</p>
+        <p className="game-status-display">
+          Which country is represented by this flag?
+        </p>
+        <ProgressBar
+          round={currentQuestionIndex + 1}
+          roundsTotal={totalQuestions}
+        />
+        <div className="question-container">
+          <div className="flag">
+            <img
+              src={`https://flagcdn.com/80x60/${currentCountry.ISOCode}.png`}
+              alt={currentCountry.name}
+            />
+          </div>
+          <div className="answers-container">
+            {answerOptions.map((country, index) => (
+              <button
+                key={index}
+                className={`answer-button ${selectedAnswer !== null &&
+                  (country === currentCountry ? 'correct' : 'incorrect')
+                  }`}
+                onClick={() =>
+                  handleAnswerClick(
+                    country === currentCountry,
+                    country.name
+                  )
+                }
+                disabled={selectedAnswer !== null}
+              >
+                {country.name}
+              </button>
+            ))}
+          </div>
         </div>
+        <button
+          className="btn"
+          onClick={nextQuestion}
+          disabled={selectedAnswer === null}
+        >
+          Next
+        </button>
       </div>
-      <p className="game-status-display">
-        Which country is represented by this flag?</p>
-      <ProgressBar round={currentQuestionIndex + 1} roundsTotal={totalQuestions} />
-      <div className="question-container">
-        <div className="flag">
-          <img
-            src={`https://flagcdn.com/80x60/${currentCountry.ISOCode}.png`}
-            alt={currentCountry.name}
-          />
-        </div>
-        <div className="answers-container">
-          {answerOptions.map((country, index) => (
-            <button
-            key={index}
-            className={`answer-button ${selectedAnswer !== null ? (country === currentCountry ? 'correct' : 'incorrect') : ''}`}
-            onClick={() => handleAnswerClick(country === currentCountry, country.name)}
-            disabled={selectedAnswer !== null}
-          >
-            {country.name}
-          </button>
-          ))}
-        </div>
-      </div>
-      <button
-        className="btn"
-        onClick={nextQuestion}
-        disabled={selectedAnswer === null}
-      >
-        Next
-      </button>
-    </div>
-  );
+    );
+  };
 
   const renderEndScreen = () => (
     <div className="game-container">
@@ -182,12 +191,16 @@ function App() {
           <p className="score-num">{totalQuestions}</p>
         </div>
       </div>
-      <p className="final-score">Your Final Score: {Math.round((score / totalQuestions) * 100)}%</p>
+      <p className="final-score">
+        Your Final Score: {Math.round((score / totalQuestions) * 100)}%
+      </p>
       {score === totalQuestions && (
         <p className="congratulations">Perfect Score! Well Done!</p>
       )}
       {score < totalQuestions && (
-        <p className="encouragement">Keep Going! You can do better next time.</p>
+        <p className="encouragement">
+          Keep Going! You can do better next time.
+        </p>
       )}
       <div className="button-container">
         <button className="btn start" onClick={restartGame}>
@@ -216,12 +229,20 @@ function App() {
           <tbody>
             {questionDetails.map((detail, index) => (
               <tr key={index}>
-                <td><img
-                  src={`https://flagcdn.com/40x30/${detail.question.ISOCode}.png`}
-                  alt={detail.question.name}
-                /></td>
+                <td>
+                  <img
+                    src={`https://flagcdn.com/40x30/${detail.question.ISOCode}.png`}
+                    alt={detail.question.name}
+                  />
+                </td>
                 <td>{detail.selectedAnswer}</td>
-                <td className={detail.userAnswer === 'Correct!' ? 'correct-answer' : 'incorrect-answer'}>
+                <td
+                  className={
+                    detail.userAnswer === 'Correct!'
+                      ? 'correct-answer'
+                      : 'incorrect-answer'
+                  }
+                >
                   {detail.userAnswer}
                 </td>
                 <td>{detail.correctAnswer}</td>
@@ -241,14 +262,22 @@ function App() {
     </div>
   );
 
-  return (
-    <div className="screen">
-      {screen === 'start' && renderStartScreen()}
-      {screen === 'question' && renderQuestionScreen()}
-      {screen === 'end' && renderEndScreen()}
-      {screen === 'detail' && renderDetailScreen()}
-    </div>
-  );
-}
+  const renderContent = () => {
+    switch (screen) {
+      case 'start':
+        return renderStartScreen();
+      case 'question':
+        return renderQuestionScreen();
+      case 'end':
+        return renderEndScreen();
+      case 'detail':
+        return renderDetailScreen();
+      default:
+        return null;
+    }
+  };
+
+  return <div className="screen">{renderContent()}</div>;
+};
 
 export default App;
